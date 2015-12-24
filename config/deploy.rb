4c0e7c5bb84edd2c8b04162b528898db272720dc
Shafeeq Kunnakkadan <shafeeq94@gmail.com>
2015-12-24 21:15:57 +0530

    Move deploy scripts to correct locations

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,25 +2,29 @@
 lock '3.4.0'
 
 set :application, 'Kek'
-set :repo_url, 'git@bitbucket.org:ershadk/kek.git'
+set :repo_url, 'https://bitbucket.org/ershadk/kek.git'
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.2'
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deployer/apps/kek'
+set :deploy_to, '/data/apps/kek'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
-# set :format, :pretty
+set :format, :pretty
 
 # Default value for :log_level is :debug
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
@@ -32,7 +36,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
