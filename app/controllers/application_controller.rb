8056e26bdbf81d43131525b3ee7ec27793fe3565
Ershad Kunnakkadan <ershad92@gmail.com>
2016-11-20 15:43:00 +0530

    Enable force_ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  force_ssl if: :ssl_configured?
+
   def authenticate_user!
     if !user_signed_in?
       render status: :unauthorized, text: 'Unauthorized. Please sign in.'
     elsif current_user && !current_user.approved?
       render status: :unauthorized, text: 'You account must be approved to modify contents on the site. Please contact hello@kek.org.in'
     end
+  end
+
+  private
+
+  def ssl_configured?
+    !Rails.env.development?
   end
 end
