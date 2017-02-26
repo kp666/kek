f7b9fe380b0e38b839f697fc5b00f96568d312b8
kp <kpisgod@gmail.com>
2017-02-26 13:17:22 +0530

    refactor peoples controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    @stories = Story.tagged_with("project").reorder('pinned desc','created_at desc').all
+    @stories = Story.tagged_with("project").reorder('pinned desc','created_at desc')
     render 'stories/index'
   end
 end
